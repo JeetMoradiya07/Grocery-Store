4a8f8c48b3a96dbbdc8b03ad2379e165ed2e9efd
Hillitaliya9999 <hillitaliya9999@gmail.com>
2024-10-14 17:20:48 +0530

    add

--- src/Components/Cart/Cart_Product.jsx
+++ src/Components/Cart/Cart_Product.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import styles from "./Cart_Product.module.css";
+import styles from "./Cart_Product.module.scss";
 // import pexels from "./assets/pexels-karolina-grabowska-5650016.jpg";
 import fernando from "../../assets/Images/fernando-andrade-potCPE-Cw8A-unsplash.jpg";
+import { IoIosAdd } from "react-icons/io";
+import { GrFormSubtract } from "react-icons/gr";
 export default function Cart_Product() {
     return (
         <>
             <div className={[`${styles.product_detail}`]}>
                 <div className={[`${styles.product_img}`]}>
-                <img src={fernando} alt="" />
+                    <img className={[`${styles.product_image}`]} src={fernando} alt="" />
                 </div>
-                <div className={[`${styles.product_info}`]}></div>
+                <div className={[`${styles.product_info}`]}>
+                    <div className={[`${styles.product_data}`]}>
+                        <div className={[`${styles.product_name}`]}>
+                            <h2>OUR LEGACY - With correction</h2>
+                            <h4>Size - Medium</h4>
+                        </div>
+                        <div className={[`${styles.product_remove}`]}>
+                            <a href="">Remove</a>
+                        </div>
+                    </div>
+                    <div className={[`${styles.product_name2}`]}>
+                        <div className={[`${styles.product_quantity}`]}>
+                            <div className={[`${styles.product_quantity_subtraction}`]}>
+                                <GrFormSubtract />
+                            </div>
+                            <div className={[`${styles.product_quantity_show}`]}>
+                                <h4>2</h4>
+                            </div>
+                            <div className={[`${styles.product_quantity_addition}`]}>
+                                <IoIosAdd />
+                            </div>
+                        </div>
+                        <div className={[`${styles.product_price}`]}>
+                            <h4>$399</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
