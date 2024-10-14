62ababacecad7eedf9ca5c7cba8635ebccbb2256
Jeet Moradiya <moradiya.jeet.00@gmail.com>
2024-10-14 17:32:08 +0530

    Changes

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import styles from "./Cart.module.scss";
-import { IoClose } from "react-icons/io5";
 import Cart_Product from "./Cart_Product";
 
-export default function Cart({ onClose }) {
+export default function Cart({onClose}) {
     return (
         <>
             <div className={styles.overlay} onClick={onClose} /> {/* Overlay that closes the cart */}
             <div className={styles.Cart}>
-                <button onClick={onClose} className={styles.closeButton}>
-                    <IoClose />
-                </button>
-
                 <div className={styles.nev}>
                     <h2>Cart</h2>
-                    <a href="">Close</a>
+                    <button onClick={onClose}>Close</button>
                 </div>
-                <Cart_Product/>
+                <Cart_Product />
             </div>
         </>
     );
